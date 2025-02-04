7b7eeee96d1b00c3bf321a75ae9321681498d181
ImTheJeff <imthejeff74@gmail.com>
2025-02-04 17:02:23 +0100

    Delete component.js

--- component.js
+++
@@ -1,9 +0,0 @@
-// class gameObject {
-//   constructor(width, height, x, y) {
-//     this.width = width;
-//     this.height = height;
-//     this.x = x;
-//     this.y = y;
-//     Engine.gameObjects.push(this);
-//   }
-// }
